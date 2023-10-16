4225bd47fc893519c826aec7374854f2366d3e9e
m <mtsekov@zohomail.eu>
2023-10-16 13:22:16 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">Smart shopping starts here</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/Navbar.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const navIcons = [
+  {
+    src: "/assets/icons/search.svg",
+    alt: "search",
+  },
+  {
+    src: "/assets/icons/black-heart.svg",
+    alt: "heart",
+  },
+  {
+    src: "/assets/icons/user.svg",
+    alt: "user",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Track <span className="text-primary">Prices</span>
+          </p>
+        </Link>
+        <div className="flex items-center gap-5">
+          {navIcons.map((i) => (
+            <Image
+              key={i.alt}
+              src={i.src}
+              alt={i.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
