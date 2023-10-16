3cca7200d7e417fb4ada542729bcb5bec7ea4568
m <mtsekov@zohomail.eu>
2023-10-16 17:38:33 +0300

    Home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
+
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
-            <p className="small-text">Smart shopping starts here</p>
+            <p className="small-text">
+              Smart shopping starts here:
+              <Image
+                src={"./assets/icons/arrow-right.svg"}
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> Prices App</span>
+            </h1>
+            <p className="mt-6">
+              Powerfull, self-driven product and growth analytics to help you
+              convert, engage and retain more.
+            </p>
+            <Searchbar />
           </div>
+          HERO CAROUSEL
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-name">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {["Book", "Sneakers", "Apple"].map((p) => p)}
         </div>
       </section>
     </>
---
+++ components/Searchbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Searchbar = () => {
+  const handleSubmit = () => {};
+
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}></form>
+  );
+};
+
+export default Searchbar;
